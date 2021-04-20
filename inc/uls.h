27ddba150ee6a647fe05fcb2c22724b2595f4f18
pashachernushenko <pashachernushenko@gmail.com>
2021-04-20 10:32:45 +0300

    How to save data tree

--- inc/uls.h
+++ inc/uls.h
@@ -1,2 +1,19 @@
 #include <stdio.h>
 #include "libmx.h"
+
+//structure, which contains file/directory properties
+typedef struct  s_fstat {
+    /*some data*/
+    int size;
+    char *name;
+}               t_fstat;
+
+//element of linked-list tree
+typedef struct  s_node {
+    struct s_fstat *fstat;
+//pointer to next recursive level
+//if node element is a directory, points to next level linked list
+//if node element is a file, points to NULL
+    struct s_node *level;
+    struct s_node *next;
+}               t_node;
