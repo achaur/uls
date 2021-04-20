354c592be0da9bc25165cacc11af2839235dede6
achaur <achaur@student.ucode.world>
2021-04-20 18:25:35 +0300

    added libraries

--- inc/uls.h
+++ inc/uls.h
@@ -1,2 +1,16 @@
-#include <stdio.h>
 #include "libmx.h"
+
+#include <stdio.h>
+#include <time.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/ioctl.h>
+#include <sys/stat.h>
+#include <sys/acl.h>
+#include <sys/xattr.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <grp.h>
+#include <pwd.h>
+#include <sys/errno.h>
