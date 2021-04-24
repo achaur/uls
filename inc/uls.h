5be9fe22c82adbed8a434fc74cc7557be777f1d4
Anastasiia Chaur <achaur@e1r3p9.unit.ua>
2021-04-24 12:23:00 +0300

    added flags structure

--- inc/uls.h
+++ inc/uls.h
@@ -12,6 +12,23 @@
 //for dir functions
 #include <dirent.h>
 
+typedef struct s_flags {
+    bool r;/*SORTING FLAGS TO IMPLEMENT*/
+    bool S;
+    bool t;
+    bool u;
+    bool c;
+    bool U;
+    bool l;/*PRINT FLAGS TO IMPLEMENT*/
+    bool a;
+    bool A;
+    bool G;
+    bool h;
+    bool i;
+    bool n;
+    bool p;
+    bool one;
+}              t_flags;
 
 //structure which holds file tree
 typedef struct  s_file {
